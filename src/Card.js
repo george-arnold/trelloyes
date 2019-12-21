ad53c80989813fd1d8f2f34ea476c494749b1ee3
George Arnold <george.arnold.88@gmail.com>
2019-12-20 19:34:26 -0500

    set up prettier

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,13 @@
 import React from 'react';
-//import react
 import './Card.css';
-//import relevant CSS
+
 function Card(props) {
-  return(
-    <div className='Card'>
-      <h2> {props.title}</h2>
+  return (
+    <div className="Card">
+      <h2>{props.title}</h2>
       <p>{props.conent}</p>
-    </div> 
-  )
+    </div>
+  );
 }
 
 export default Card;
-
--- src/List.js
+++ src/List.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import Card from './Card'
+import Card from './Card';
 import './List.css';
 
 function List(props) {
-  render (
-    <section className='List'>
+  render(
+    <section className="List">
       <header>
         <h1>{props.header};</h1>
       </header>
-      <div className='List-cards'>
-        {props.cards.map((card) =>
-        <Card 
-        key= {card.id}
-        title= {card.title}
-        content= {card.content}
-        />)}
+      <div className="List-cards">
+        {props.cards.map(card => (
+          <Card key={card.id} title={card.title} content={card.content} />
+        ))}
       </div>
     </section>
   );
